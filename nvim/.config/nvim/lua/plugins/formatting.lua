85601d31562ed21cf8ece8edbb624d92b30889df
mikewitk <mike.witk@gmail.com>
2025-07-14 16:15:48 -0400

    Tailwind: lsp, visual indicator, sorting

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -3,10 +3,27 @@ return {
   opts = {
     formatters_by_ft = {
       html = {"prettier"},
-      javascript = { "prettier"},
+      javascript = {"prettier"},
+      javascriptreact = {"prettier"}, -- For JSX in JS files
       typescript = {"prettier"},
+      typescriptreact = {"prettier"}, -- For JSX/TSX in TS files
+      json = {"prettier"},
+      css = {"prettier"},
+      scss = {"prettier"},
       lua = {"stylua"}
-
-    }
+    },
+    formatters = {
+      prettier = {
+        -- Ensure prettier can infer the file type correctly
+        args = function(self, ctx)
+          local args = {"--stdin-filepath", ctx.filename}
+          if vim.fn.filereadable(".prettierrc") == 1 or vim.fn.filereadable(".prettierrc.json") == 1 then
+            table.insert(args, "--config-precedence")
+            table.insert(args, "prefer-file")
+          end
+          return args
+        end,
+      },
+    },
   },
 }
---
+++ nvim/.config/nvim/lua/plugins/tailwind.lua
@@ -0,0 +1,68 @@
+-- lua/plugins/tailwind.lua
+return {
+  -- Official Tailwind CSS Language Server integration
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          settings = {
+            tailwindCSS = {
+              includeLanguages = {
+                jsx = "javascriptreact",
+                tsx = "typescriptreact",
+              },
+              experimental = {
+                classRegex = {
+                  "tw`([^`]*)`", -- for `tw` template literal support (if you use it)
+                  'clsx\\(([^)]*)\\)', -- for clsx utility
+                  'cva\\(([^)]*)\\)', -- for cva utility
+                  'cn\\(([^)]*)\\)', -- for cn utility
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  -- Tailwind CSS tooling and colorizer (compatible with blink.cmp)
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {
+      document_color = {
+        enabled = true, -- can be toggled by commands
+        kind = "inline", -- "inline" | "foreground" | "background"
+        inline_symbol = "●", -- only used in inline mode
+        debounce = 200, -- in milliseconds, only applied in insert mode
+      },
+      conceal = {
+        enabled = false, -- can be toggled by commands
+        min_length = nil, -- only conceal classes exceeding the provided length
+        symbol = "󱏿", -- only a single character is allowed
+        highlight = {
+          fg = "#38BDF8", -- style the symbol with a custom color
+        },
+      },
+      custom_filetypes = {}, -- additional filetypes where tailwind classes should be detected
+    },
+    config = function(_, opts)
+      require("tailwind-tools").setup(opts)
+      
+      -- Auto-sort Tailwind classes on save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = {"*.html", "*.jsx", "*.tsx", "*.js", "*.ts", "*.vue", "*.svelte"},
+        callback = function()
+          vim.cmd("TailwindSort")
+        end,
+      })
+    end,
+  },
+}
