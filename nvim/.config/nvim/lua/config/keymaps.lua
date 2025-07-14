f39a32d6eb16d7bcdbaf05cdbe78088422047b2b
mikewitk <mike.witk@gmail.com>
2025-07-14 14:46:57 -0400

    copy diagnostics to clipboard

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Yank diagonstics to clipboard
+vim.keymap.set("n", "<leader>zy", function ()
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local diagonstics = vim.diagnostic.get(0, {lnum = line - 1})
+
+  -- empty the register first
+  vim.fn.setreg('+', {}, 'V')
+
+  for _, diagnostic in ipairs(diagonstics) do
+    vim.fn.setreg('+', vim.fn.getreg('+') .. diagnostic["message"], 'V')
+  end
+end, {desc = "Copy current line diagonstics"})
